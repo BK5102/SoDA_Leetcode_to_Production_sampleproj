5c11a7f0a68f575d56d7a185e37a2c1c00809cae
Bhavana Kannan <bhavanakannan06@gmail.com>
2025-08-03 21:30:33 -0700

    feat: add libraries for routing and schema. feat: add json for vercel

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,2 +1,22 @@
 ## All route definitions and endpoints
 
+## import request and response pydantic models that ensure input/output validation
+## create database session, SQLModel table with input/output submissions and record time of API calls
+
+from fastapi import APIRouter
+from app.schema_setup.pydantic_schema import InputModel, OutputModel ## Pydantic models
+from app.db_session_setup.session import get_session ## database interaction
+from app.sqlmodel_setup.sqlmodel_submission import Submission
+
+from app.core_functions.leetcode_problem import twoSum
+
+from sqlmodel import Session
+from datetime import datetime
+
+
+
+router = APIRouter() 
+
+@router.post("/problem", response_model=OutputModel)
+def problem_endpoint(problem: InputModel, session: Session = get_session):
+    print(problem)
